fa36f2e4b11615ebe4842fe1b46ac23ac16dd3ed
JAMES GAMBRAH <jamesgambrah@gmail.com>
2025-08-01 14:01:01 +0000

    studio-4576708489:~/studio{master}$ npm run dev
    > nextn@0.1.0 dev
    > nex

--- next.config.ts
+++ next.config.ts
@@ -27,6 +27,11 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_FIREBASE_APP_ID: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
     NEXT_PUBLIC_GEMINI_API_KEY: process.env.GEMINI_API_KEY,
   },
+  experimental: {
+    allowedDevOrigins: [
+      'https://9003-firebase-studio-1754053472741.cluster-ikslh4rdsnbqsvu5nw3v4dqjj2.cloudworkstations.dev',
+    ],
+  },
 };
 
 export default nextConfig;
