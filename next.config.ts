bbc7369e191671b21251f45d72c4a5c38761a9d6
JAMES GAMBRAH <jamesgambrah@gmail.com>
2025-09-23 15:55:31 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'api.qrserver.com',
+            }
+        ]
+    }
 }
 
 export default nextConfig
