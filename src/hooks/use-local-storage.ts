92138c49d7d743d4a2aa373727e4d85301eb98e2
JAMES GAMBRAH <jamesgambrah@gmail.com>
2025-09-25 16:05:39 +0000

    Try fixing this error: `Runtime Error: Hydration failed because the serv

--- src/hooks/use-local-storage.ts
+++ src/hooks/use-local-storage.ts
@@ -4,33 +4,25 @@
 import { useState, useEffect } from 'react';
 
 function useLocalStorage<T>(key: string, initialValue: T): [T, React.Dispatch<React.SetStateAction<T>>] {
-  // Get from local storage then
-  // parse stored json or return initialValue
-  const readValue = () => {
-    if (typeof window === 'undefined') {
-      return initialValue;
-    }
+  // State to store our value
+  // Pass initial state function to useState so logic is only executed once
+  const [storedValue, setStoredValue] = useState<T>(initialValue);
+
+  useEffect(() => {
+    // This code only runs on the client-side, after the component has mounted.
     try {
       const item = window.localStorage.getItem(key);
-      return item ? JSON.parse(item) : initialValue;
+      // Parse stored json or if none return initialValue
+      setStoredValue(item ? JSON.parse(item) : initialValue);
     } catch (error) {
+      // If error also return initialValue
       console.warn(`Error reading localStorage key “${key}”:`, error);
-      return initialValue;
+      setStoredValue(initialValue);
     }
-  };
+  }, [key, initialValue]);
 
-  const [storedValue, setStoredValue] = useState<T>(readValue);
 
-  // Return a wrapped version of useState's setter function that ...
-  // ... persists the new value to localStorage.
   const setValue: React.Dispatch<React.SetStateAction<T>> = (value) => {
-    // Prevent build error "window is undefined" but keep keep working
-    if (typeof window == 'undefined') {
-      console.warn(
-        `Tried setting localStorage key “${key}” even though environment is not a client`,
-      );
-    }
-
     try {
       // Allow value to be a function so we have same API as useState
       const valueToStore =
@@ -38,17 +30,14 @@ function useLocalStorage<T>(key: string, initialValue: T): [T, React.Dispatch<Re
       // Save state
       setStoredValue(valueToStore);
       // Save to local storage
-      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      if (typeof window !== 'undefined') {
+        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      }
     } catch (error) {
       console.warn(`Error setting localStorage key “${key}”:`, error);
     }
   };
   
-  useEffect(() => {
-    setStoredValue(readValue());
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   return [storedValue, setValue];
 }
 
