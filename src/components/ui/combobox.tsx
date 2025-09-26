983b85e0779f8800492a7cea34fe6ba9d95285cb
JAMES GAMBRAH <jamesgambrah@gmail.com>
2025-09-26 15:40:16 +0000

    yes, the list of admitted patients appear but it's like the list has bee

--- src/components/ui/combobox.tsx
+++ src/components/ui/combobox.tsx
@@ -1,4 +1,3 @@
-
 "use client"
 
 import * as React from "react"
@@ -63,9 +62,12 @@ export function Combobox({
               {options.map((option) => (
                 <CommandItem
                   key={option.value}
-                  value={option.value}
-                  onSelect={(currentValue) => {
-                    onChange(currentValue === value ? "" : currentValue)
+                  value={option.label}
+                  onSelect={(currentLabel) => {
+                    const selectedOption = options.find(opt => opt.label.toLowerCase() === currentLabel.toLowerCase());
+                    if (selectedOption) {
+                      onChange(selectedOption.value === value ? "" : selectedOption.value)
+                    }
                     setOpen(false)
                   }}
                 >
