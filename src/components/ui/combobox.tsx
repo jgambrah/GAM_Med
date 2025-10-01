ba67659c60e538e16a75fc0d192312a3b4f3d5ac
JAMES GAMBRAH <jamesgambrah@gmail.com>
2025-10-01 07:58:16 +0000

    IT IS STILL THE SAME, I CAN'T THE LEDGER OF INTEREST IN THE LIST OF LEDG

--- src/components/ui/combobox.tsx
+++ src/components/ui/combobox.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -17,6 +18,7 @@ import {
   Popover,
   PopoverContent,
   PopoverTrigger,
+  PopoverPortal,
 } from "@/components/ui/popover"
 
 interface ComboboxProps {
@@ -53,39 +55,41 @@ export function Combobox({
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-[--radix-popover-trigger-width] p-0">
-        <Command>
-          <CommandInput placeholder={searchPlaceholder} />
-          <CommandList>
-            <CommandEmpty>{notFoundText}</CommandEmpty>
-            <CommandGroup>
-              {options.map((option) => (
-                <CommandItem
-                  key={option.value}
-                  value={option.label}
-                  onSelect={(currentLabel) => {
-                    const selectedValue = options.find(
-                      (opt) => opt.label.toLowerCase() === currentLabel.toLowerCase()
-                    )?.value;
-                    if (selectedValue) {
-                        onChange(selectedValue === value ? "" : selectedValue);
-                    }
-                    setOpen(false)
-                  }}
-                >
-                  <Check
-                    className={cn(
-                      "mr-2 h-4 w-4",
-                      value === option.value ? "opacity-100" : "opacity-0"
-                    )}
-                  />
-                  {option.label}
-                </CommandItem>
-              ))}
-            </CommandGroup>
-          </CommandList>
-        </Command>
-      </PopoverContent>
+      <PopoverPortal>
+        <PopoverContent className="w-[--radix-popover-trigger-width] p-0" align="start">
+          <Command>
+            <CommandInput placeholder={searchPlaceholder} />
+            <CommandList>
+              <CommandEmpty>{notFoundText}</CommandEmpty>
+              <CommandGroup>
+                {options.map((option) => (
+                  <CommandItem
+                    key={option.value}
+                    value={option.label}
+                    onSelect={(currentLabel) => {
+                      const selectedValue = options.find(
+                        (opt) => opt.label.toLowerCase() === currentLabel.toLowerCase()
+                      )?.value;
+                      if (selectedValue) {
+                          onChange(selectedValue === value ? "" : selectedValue);
+                      }
+                      setOpen(false)
+                    }}
+                  >
+                    <Check
+                      className={cn(
+                        "mr-2 h-4 w-4",
+                        value === option.value ? "opacity-100" : "opacity-0"
+                      )}
+                    />
+                    {option.label}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </CommandList>
+          </Command>
+        </PopoverContent>
+      </PopoverPortal>
     </Popover>
   )
 }
