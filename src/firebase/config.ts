366e073e52d0528001b2f3939396103349a2d64d
JAMES GAMBRAH <jamesgambrah@gmail.com>
2026-01-19 09:41:28 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "medflow-gh",
+  "appId": "1:686315337824:web:c03fdd8e7f0cdf50602b05",
+  "apiKey": "AIzaSyBJkA1W18XFXJ7NHu2yGxiifscLNqj-lfs",
+  "authDomain": "medflow-gh.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "686315337824"
+};
